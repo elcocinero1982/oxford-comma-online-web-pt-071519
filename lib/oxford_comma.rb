0bad691091d27603cf7c6561f1e327f50f4bbdf2
jose luis <cortezluisjose@gmail.com>
2019-07-24 03:14:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
-["kiwi", "durian", "starfruit", "mangos and dragon fruits"] = oxford_comma.split
+array = ["kiwi", "durian", "starfruit", "mangos and dragon fruits"] 
 puts elements.length
 puts elements
 
